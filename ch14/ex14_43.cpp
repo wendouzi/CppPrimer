c6cf3f8a4f4e1740fa290c143ce4febc00fc3b54
wendouzi <735196447@qq.com>
2017-03-03 21:39:32 +0800

    Update ex14_43.cpp

--- ch14/ex14_43.cpp
+++ ch14/ex14_43.cpp
@@ -8,7 +8,7 @@
 //!
 //! Exercise 14.43:
 //! Using library function objects, determine whether a given int value is
-//! divisible by any element in a container of ints.
+//! divisible by all element in a container of ints.
 //!
 
 #include <iostream>
